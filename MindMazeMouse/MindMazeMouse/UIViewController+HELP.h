db75b98c7f7d7a49124425980a22857a39dd44c0
niba <>
2024-12-25 16:45:02 +0800

    ini a

---
+++ MindMazeMouse/MindMazeMouse/UIViewController+HELP.h
@@ -0,0 +1,16 @@
+//
+//  UIViewController+HELP.h
+//  MindMazeMouse
+//
+//  Created by jin fu on 2024/12/25.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIViewController (HELP)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MindMazeMouse/MindMazeMouse/UIViewController+HELP.m
@@ -0,0 +1,12 @@
+//
+//  UIViewController+HELP.m
+//  MindMazeMouse
+//
+//  Created by jin fu on 2024/12/25.
+//
+
+#import "UIViewController+HELP.h"
+
+@implementation UIViewController (HELP)
+
+@end
